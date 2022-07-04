332b94884b6900e38b179d0801e58b550197fc92
Seng Kit Mun <cellkit11@gmail.com>
2022-07-04 19:50:05 -0400

    created start.html

---
+++ startscript.js
@@ -0,0 +1,7 @@
+const playButton = document.getElementById('playBtn')
+
+playButton.addEventListener('click', () => {
+  console.log('Clicked already!')
+  window.location.href =
+    'file:///Users/sengkitmun/ga_seir/projects/Greedy-Snake/game.html'
+})
